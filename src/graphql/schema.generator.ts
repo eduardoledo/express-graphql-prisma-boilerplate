3ef19eeb09327bd2c3d4dd2d7245608caa678a11
Eduardo Ledo <eduardo.ledo@gmail.com>
2021-08-27 00:07:46 -0300

    Renamed @authorized to @hasRole

--- src/graphql/schema.generator.ts
+++ src/graphql/schema.generator.ts
@@ -2,22 +2,22 @@ import { makeExecutableSchema } from '@graphql-tools/schema';
 import { compose } from '../helpers/compose';
 import { generateTypeDefinitions, TypeDefinition } from "./type.generator";
 import { generateResolverDefinitions } from './resolver.generator';
-import { authorizedDirectiveTransformer, getAuthorizedDirective } from './directives/authorized.type';
-
+import { hasRoleDirectiveTransformer, hasRoleDirectiveTypeDef } from './directives/hasRole';
 
 export const generateSchema = async (authenticated = false) => {
 
     // Collect resolvers and type definitions based on auth
     const resolvers = authenticated ? await generateResolverDefinitions(TypeDefinition.Authenticated) : await generateResolverDefinitions(TypeDefinition.Unauthenticated);
     const typeDefs = authenticated ? await generateTypeDefinitions(TypeDefinition.Authenticated) : await generateTypeDefinitions(TypeDefinition.Unauthenticated);
-    const authorizedDirective = (await getAuthorizedDirective());
+    
+    const hasRoleDirective = (await hasRoleDirectiveTypeDef());
 
     const schema = compose(
-        (await authorizedDirectiveTransformer),
+        (await hasRoleDirectiveTransformer),
         makeExecutableSchema
     )({
         typeDefs: [
-            authorizedDirective,
+            hasRoleDirective,
             typeDefs,
         ],
         resolvers,
