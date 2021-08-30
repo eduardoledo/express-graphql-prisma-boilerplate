8d939c647b77c941aa0564fcba173b88ffa2ba7e
Eduardo Ledo <eduardo.ledo@gmail.com>
2021-08-30 13:20:12 -0300

    Removed unnecessary code

--- src/graphql/authenticated/book/book.mutation.service.ts
+++
@@ -1,6 +0,0 @@
-export default class BookMutationService {
-    deleteById(id: number): string {
-        // Some delete logic
-        return "Deleted successfully";
-    }
-}
--- src/interfaces/middleware.check.interface.ts
+++
@@ -1,6 +0,0 @@
-import { MiddlewareType } from "../middlewares/middleware.check";
-
-export interface IMiddlewareCheckInputType {
-    type: MiddlewareType,
-    roles?: string[]
-}
